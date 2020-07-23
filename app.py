14b6a999ee2033a8100b4f8bda526a4ef4bd50e4
Alex <htcones51@gmail.com>
2020-07-23 22:06:15 +0300

    Updated runtime.txt with python

--- app.py
+++ app.py
@@ -8,19 +8,15 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 
-app = Flask(__name__)           #Creating an instance of Flask class with name(name is equal to __main__ as it runs itself and not imported)
+app = Flask(__name__)                                                   #Creating an instance of Flask class with name(name is equal to __main__ as it runs itself and not imported)
 
-app.secret_key = 'sasha'       #Anything that requires encryption (for safe-keeping against tampering by attackers)
-                               #requires the secret key to be set. For just Flask itself, that 'anything' is the Session object,#
-                               #but other extensions can make use of the same secret.
+app.secret_key = 'sasha'                                                #Anything that requires encryption (for safe-keeping against tampering by attackers)
+                                                                        #requires the secret key to be set. For just Flask itself, that 'anything' is the Session object,#
+                                                                        #but other extensions can make use of the same secret.
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-jwt = JWT(app, authenticate, identity)  # /authorization decorator, passes the parameters to security.py for processing
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
+jwt = JWT(app, authenticate, identity)                              # /authorization decorator, passes the parameters to security.py for processing
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList,'/stores')
