b48ced7b2142754ca52ed01a56491c44acafac20
Likh-Alex <67925366+Likh-Alex@users.noreply.github.com>
2020-07-28 20:44:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -30,11 +30,9 @@ api.add_resource(UserRegister, '/register')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
+    app.run(port=5000)
 
-    if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
+  
 
     
-    app.run(port=5000)
+    
